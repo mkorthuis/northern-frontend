2e5817986512d9c7c2ef49de2ea8b36de63e333c
Michael Korthuis <michael.korthuis@gmail.com>
2025-04-16 22:21:36 -0400

    Removed v1 functionality

--- src/features/survey/components/analysis/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as AnalysisFormDialog } from './AnalysisFormDialog';
-export { default as AnalysisQuestionFormDialog } from './AnalysisQuestionFormDialog';
-export { default as AnalysisList } from './AnalysisList';
-export { default as AnalysisPanel } from './AnalysisPanel';
-export { default as TabPanel } from './TabPanel';
-export { default as DeleteConfirmationDialog } from './DeleteConfirmationDialog';
-export { default as DeleteAnalysisDialog } from './DeleteAnalysisDialog'; 
